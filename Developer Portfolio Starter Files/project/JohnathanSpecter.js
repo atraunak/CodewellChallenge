d62040b40ba12d9e9a62fce2702baa547b77054b
not-a-cat <94775954+not-a-cat@users.noreply.github.com>
2023-05-01 22:17:01 +0530

    Delete Developer Portfolio Starter Files directory

--- Developer Portfolio Starter Files/project/JohnathanSpecter.js
+++
@@ -1,9 +0,0 @@
-const mobileNav = () =>{
-const menuButton = document.querySelector('#menu__button');
-const mobileNav = document.querySelector('.mobile__nav');
-
-menuButton.addEventListener('click', () => {
-    mobileNav.Style.display = 'flex';
-});
-}
-export default mobileNav;
